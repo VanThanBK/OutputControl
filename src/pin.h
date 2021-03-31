16cc87cc8df5462114410184eb27da9dd8d27adb
Than <jonyvantha@gmail.com>
2021-03-31 08:47:38 +0700

    v2 main board init pin

--- src/pin.h
+++ src/pin.h
@@ -1,27 +1,52 @@
 #pragma once
 
-#define RELAY_1_PIN PB7
-#define RELAY_2_PIN PB6
-#define RELAY_3_PIN PB5
-#define RELAY_4_PIN PB4
+#define D_0_PIN PA7
+#define D_1_PIN PA2
+#define D_2_PIN PA1
+#define D_3_PIN PB9
+#define D_4_PIN PB8
+#define D_5_PIN PC15
+#define D_6_PIN PC14
+#define D_7_PIN PC13
+#define D_8_PIN PA0
+#define D_9_PIN PB7
+#define D_10_PIN PB6
+#define D_11_PIN PB5
+#define D_12_PIN PB4
+#define D_13_PIN PB2
+#define D_14_PIN PB1
+#define D_15_PIN PB0
 
-#define DIGITAL_OUT_1_PIN PB9
-#define DIGITAL_OUT_2_PIN PB8
-#define DIGITAL_OUT_3_PIN PA2
-#define DIGITAL_OUT_4_PIN PA1
+#define D_0_PWM_PIN PA7
+#define D_1_PWM_PIN PA2
+#define D_2_PWM_PIN PA1
+#define D_3_PWM_PIN PB9
+#define D_4_PWM_PIN PB8
+#define D_5_PWM_PIN 255
+#define D_6_PWM_PIN 255
+#define D_7_PWM_PIN 255
+#define D_8_PWM_PIN PA0
+#define D_9_PWM_PIN PB7
+#define D_10_PWM_PIN PB6
+#define D_11_PWM_PIN 255
+#define D_12_PWM_PIN 255
+#define D_13_PWM_PIN 255
+#define D_14_PWM_PIN PB1
+#define D_15_PWM_PIN PB0
 
-#define PWM_OUT_1_PIN PB0
-#define PWM_OUT_2_PIN PB1
+#define I_0_PIN PA11
+#define I_1_PIN PA8
+#define I_2_PIN PB15
+#define I_3_PIN PB14
+#define I_4_PIN PB13
+#define I_5_PIN PB12
+#define I_6_PIN PB11
+#define I_7_PIN PB10
 
-#define SERVO_1_PIN PA6
-#define SERVO_2_PIN PA7
+#define E_STOP1_PIN PA12
+#define E_STOP2_PIN PA15
 
-#define DIGITAL_IN_6_PIN PB15
-#define DIGITAL_IN_5_PIN PB14
-#define DIGITAL_IN_4_PIN PB13
-#define DIGITAL_IN_1_PIN PB10
-#define DIGITAL_IN_2_PIN PB11
-#define DIGITAL_IN_3_PIN PB12
-
-#define ANALOG_IN_1_PIN PA4
-#define ANALOG_IN_2_PIN PA5
+#define A_0_PIN PA5
+#define A_1_PIN PA6
+#define A_2_PIN PA3
+#define A_3_PIN PA4
